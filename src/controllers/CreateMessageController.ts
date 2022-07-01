930332f04e0b18b81860d4f16b69220ba13fce1d
Manasses Francisco <manassensevani@gmail.com>
2022-07-01 20:20:08 +0100

    First Commit

---
+++ src/controllers/CreateMessageController.ts
@@ -0,0 +1,19 @@
+import {Request,Response} from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
+
+class CreateMessageController{
+
+    async handle(request:Request,response:Response){
+
+        const {user_id}=request;
+        const {message} = request.body;
+
+        const service= new CreateMessageService();
+
+        const result = await service.execute(message,user_id);
+
+        response.status(200).json(result );
+    }
+}
+
+export {CreateMessageController}
